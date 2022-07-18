d0c2d5f193596256562fb342e21940f973e586b2
emher <emil.h.eriksen@gmail.com>
2022-07-18 11:16:50 +0200

    update id

--- transforms/operator_log.py
+++ transforms/operator_log.py
@@ -4,11 +4,11 @@ from dash_extensions.enrich import DashProxy, html, Input, Operator, OperatorTra
 
 app = DashProxy(transforms=[OperatorTransform()])
 app.layout = html.Div([
-    html.Button("Click me", id="btn"),
+    html.Button("Record click", id="record"),
     dmc.List(id="log", children=[]),
 ])
 
-@app.callback(OperatorOutput("log", "children"), Input("btn", "n_clicks"))
+@app.callback(OperatorOutput("log", "children"), Input("record", "n_clicks"))
 def append_to_log(n_clicks):
     log_entry = f"{datetime.datetime.now().isoformat()}: BUTTON CLICKED (n_clicks={n_clicks})"
     return Operator().list.append(dmc.ListItem(log_entry))
